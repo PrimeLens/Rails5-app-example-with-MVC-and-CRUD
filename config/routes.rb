1e252023d7f02ef6d593c451326a6e2483fc40e7
PrimeLens <jhoedavis@gmail.com>
2017-01-19 13:42:12 -0500

    fix typo in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # use the films controller
   resources :films	
-  root 'films/index'
+  root 'films#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
